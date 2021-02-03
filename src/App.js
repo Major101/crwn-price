b0d438cada5fc3c526abb6b10b63d2cea8a15d81
Major101 <majorMail101@gmail.com>
2021-02-03 20:15:41 +0500

    created header component and collection item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route} from "react-router-dom";
-
-const HatsPage = () => {
-    return <div>
-        <h1>Hats Page</h1>
-    </div>
-}
+import {Switch, Route} from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route path='/hats' component={HatsPage}></Route>
+      <Header/>
+      <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ReactComponent as Logo} from '../../assests/crown.svg';
+import {Link} from 'react-router-dom';
+import './header.styles.scss';
+
+const Header = () => {
+    return <div className='header'>
+        <Logo className='logo-conrainer'/>
+
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option'>CONTACT</Link>
+        </div>
+    </div>
+}
+
+export default Header;
